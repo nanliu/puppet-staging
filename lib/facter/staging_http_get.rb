42f19394aac38bfcad9b7caf5686cec4c912ef1d
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2015-09-01 10:33:01 +0200

    Cleanup staging_http_get.rb
    Use some ruby high-level iterators to simplify logic of 'which' and 'staging_http_get'.

--- lib/facter/staging_http_get.rb
+++ lib/facter/staging_http_get.rb
@@ -1,29 +1,17 @@
 Facter.add("staging_http_get") do
   setcode do
 
-    fact = nil
-
     which = lambda do |cmd|
-      result = nil
       exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-        exts.each do |ext|
-          exe = File.join(path, "#{cmd}#{ext}")
-          result = exe if File.executable? exe
-          break if result
+      locations = ENV['PATH'].split(File::PATH_SEPARATOR).flat_map do |path|
+        exts.map do |ext|
+          File.join(path, "#{cmd}#{ext}")
         end
-        break if result
       end
-      result
+      locations.find { |loc| File.executable? loc }
     end
 
-    ['powershell', 'curl', 'wget'].each do |cmd|
-      available = which.call(cmd)
-      fact = available ? cmd : nil
-      break if fact
-    end
-
-    fact
+    ['powershell', 'curl', 'wget'].find { |cmd| which.call(cmd) }
 
   end
 end
